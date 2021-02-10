176106742ef2f47acf73d2da2023fb2ddc982675
Zekel97 <szandreas97@gmail.com>
2021-02-10 11:56:53 +0100

    Miglioramento bottoni

--- assets/main.ts
+++ assets/main.ts
@@ -1,21 +1,47 @@
 import './main.scss';
 
-console.log('Cara MEP, questo è ciò che sono riuscito a fare per il momento. Ho ancora da allenarmi con lo scripting però sono sicuro di migliorare in poco tempo. Giusto il tempo per un caffè. O due. O dieci.');
+//----------- VARIABLES 
+    const ghostButton = document.getElementById("spirits");
+    ghostButton!.addEventListener("click", posts);
 
-document.addEventListener('DOMContentLoaded', function() {
-    var messaggio = document.querySelector('.js-user-rating');
-    console.log(messaggio?.attributes[1].value);
-});
+    const messageButton = document.getElementById("message");
+    messageButton!.addEventListener("click", messaggio);
 
-const button = document.getElementById("ciao");
-button!.addEventListener("click", onMouseClick);
-let val=0;
+    let data;
+    let count = 0;
 
-function onMouseClick()
-{
-    val++;
-    console.log("-----------------------------------------------");
-    console.log(" Chiedo scusa per l'utilizzo del file 'config/packages/twig.yaml' ");
-    console.log(" Ma era il modo più immediato che avevo per usare il template creato ");
-    console.log(" Un piccolo contatore delle volte che avete premuto il pulsantino: "+val);
-}
+//----------- CONSOLE LOGS
+    console.log('Cara MEP, questo è ciò che sono riuscito a fare per il momento. Ho ancora da allenarmi con lo scripting però sono sicuro di migliorare in poco tempo. Giusto il tempo per un caffè. O due. O dieci.');
+
+//----------- FUNCTIONS
+    document.addEventListener('DOMContentLoaded', function() {
+        var messaggio = document.querySelector('.js-user-auth');
+        console.log(messaggio?.attributes[1].value);
+    });
+
+
+    function posts()
+    {
+
+        fetch('http://127.0.0.1:8000/posts')
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(myJson) {
+        
+                data=myJson
+                console.log("|POST "+count+" ---------| USERID "+data[count].userId+"-------")
+                console.log("Title: "+data[count].title)
+                console.log("Body: "+data[count].body)
+                console.log("---------------------------")
+                count++
+            });
+        
+    }
+
+    function messaggio()
+    {
+            console.log(" Chiedo scusa per l'utilizzo del file 'config/packages/twig.yaml' ")
+            console.log(" Ma era il modo più immediato che avevo per popolare il template creato ")
+            console.log("-----------------------------------------------")
+    }
